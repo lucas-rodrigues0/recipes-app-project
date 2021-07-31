4cd718f25aa0ef955b13cb3a96e48ad6197832cc
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-07-31 14:27:00 -0300

    Updates

---
+++ precache-manifest.b7cb496cbda9621b9c7ea323ac72a560.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dade8ba2742bf6aeb4fdfa3ee47115b1",
+    "url": "/recipes-app-project/index.html"
+  },
+  {
+    "revision": "e510627a6c095680367f",
+    "url": "/recipes-app-project/static/css/2.c3014acc.chunk.css"
+  },
+  {
+    "revision": "2a2245941f5561e3a421",
+    "url": "/recipes-app-project/static/css/main.ac876556.chunk.css"
+  },
+  {
+    "revision": "e510627a6c095680367f",
+    "url": "/recipes-app-project/static/js/2.6d44631b.chunk.js"
+  },
+  {
+    "revision": "57fb86a905f06aed4b5091745659b335",
+    "url": "/recipes-app-project/static/js/2.6d44631b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a2245941f5561e3a421",
+    "url": "/recipes-app-project/static/js/main.539a4b08.chunk.js"
+  },
+  {
+    "revision": "0b8e046d891008656e41",
+    "url": "/recipes-app-project/static/js/runtime-main.85a82b02.js"
+  },
+  {
+    "revision": "8b36cac85fff7bf947afb794e39f1fd7",
+    "url": "/recipes-app-project/static/media/Drinks_Desktop.8b36cac8.png"
+  },
+  {
+    "revision": "2b604d014789e3ecf096fecece9b695d",
+    "url": "/recipes-app-project/static/media/Drinks_Mobile.2b604d01.png"
+  },
+  {
+    "revision": "dfab176d7a0655dc28569315007ffa50",
+    "url": "/recipes-app-project/static/media/Login_Desktop.dfab176d.png"
+  },
+  {
+    "revision": "9d19c76c7a4486acceb507ef1868007d",
+    "url": "/recipes-app-project/static/media/Login_Mobile.9d19c76c.png"
+  },
+  {
+    "revision": "e3d9c8c38d214804fd0a212a6f6c8e77",
+    "url": "/recipes-app-project/static/media/Logo_drinkIcon.e3d9c8c3.png"
+  },
+  {
+    "revision": "c6dea05249287ecf7e9ba9a5e7bb21a2",
+    "url": "/recipes-app-project/static/media/Logo_exploreIcon.c6dea052.png"
+  },
+  {
+    "revision": "1dd95ca3878fd00b685c34adbdb77d19",
+    "url": "/recipes-app-project/static/media/Logo_mealIcon.1dd95ca3.png"
+  },
+  {
+    "revision": "5be47e330670c5831183f0d7ab7d25c1",
+    "url": "/recipes-app-project/static/media/Logo_profileIcon.5be47e33.png"
+  },
+  {
+    "revision": "e10d6ef8076f00957ac29b6aa74e71ce",
+    "url": "/recipes-app-project/static/media/Logo_searchIcon.e10d6ef8.png"
+  },
+  {
+    "revision": "a59a8f824c0b05fa6e9ffadbcd18738f",
+    "url": "/recipes-app-project/static/media/Meals_Desktop.a59a8f82.png"
+  },
+  {
+    "revision": "18ebd1871aefe5d06932b48e3ab3e017",
+    "url": "/recipes-app-project/static/media/Meals_Mobile.18ebd187.png"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/recipes-app-project/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/recipes-app-project/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/recipes-app-project/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
