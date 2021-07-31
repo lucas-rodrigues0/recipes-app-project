b593868747507f2e40a30634d7fd1afd712c85d1
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-07-31 12:50:48 -0300

    Refactoring css of the favorites and finished recipes pages

--- src/components/FilterTypeBtn.jsx
+++ src/components/FilterTypeBtn.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 export default function FilterTypeBtn({ handleSelector }) {
+  const [ btnContainerSize, setContainerSize ] = useState('');
+
+  useEffect(() => {
+    if (window.innerWidth < 400) {
+      setContainerSize('col d-flex  align-items-center justify-content-center py-2');
+    } else {
+      setContainerSize('col d-flex align-items-center justify-content-center w-25 py-2');
+    }
+
+  }, []);
+
   return (
-    <div className="filter-type-btn-section">
+    <div className={ btnContainerSize } style={ { backgroundColor: 'rgb(0, 0, 0, 0.8)' } }>
       <button
         type="button"
         data-testid="filter-by-all-btn"
         value="all"
         onClick={ (e) => handleSelector(e) }
-        className="regular-button"
+        className="btn btn-outline-light px-3 py-1 mx-1"
       >
         All
       </button>
@@ -18,7 +29,7 @@ export default function FilterTypeBtn({ handleSelector }) {
         data-testid="filter-by-food-btn"
         value="food"
         onClick={ (e) => handleSelector(e) }
-        className="regular-button"
+        className="btn btn-outline-light px-3 py-1 mx-1"
       >
         Food
       </button>
@@ -27,7 +38,7 @@ export default function FilterTypeBtn({ handleSelector }) {
         data-testid="filter-by-drink-btn"
         value="drinks"
         onClick={ (e) => handleSelector(e) }
-        className="regular-button"
+        className="btn btn-outline-light px-3 py-1 mx-1"
       >
         Drinks
       </button>
